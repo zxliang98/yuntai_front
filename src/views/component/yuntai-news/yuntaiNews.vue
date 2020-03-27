ea39adb24b0404f15ea1672454057c53dbfdba2a
zxliang98 <1287520001@qq.com>
2020-03-27 19:31:39 +0800

    update

--- src/views/component/yuntai-news/yuntaiNews.vue
+++ src/views/component/yuntai-news/yuntaiNews.vue
@@ -2,7 +2,12 @@
   <div class="yuntai-news">
     <div class="news-left">
       <div class="left-top">
-        <div class="left">资讯</div>
+        <div class="left">
+          <div class="title-box">
+            <div class="title">云台·资讯</div>
+            <div class="action">更多</div>
+          </div>
+        </div>
         <div class="right">
           <el-image fit="cover" :src="importImgs.imgBanner2"></el-image>
         </div>
@@ -13,9 +18,39 @@
     </div>
     <div class="news-right">
       <div class="right-top">
-        <div class="item">在线预约订票</div>
-        <div class="item">全景虚拟游览</div>
-        <div class="item">博物馆活动</div>
+        <div class="item">
+          <div class="logo">云</div>
+          <div class="info">
+            <div class="title">
+              在线预约订票
+            </div>
+            <div class="des">
+              贴心的票务服务,让您省事省力更省心
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <div class="logo">台</div>
+          <div class="info">
+            <div class="title">
+              全景虚拟游览
+            </div>
+            <div class="des">
+              随时随地,为您提供身临其境的游览体验
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <div class="logo">山</div>
+          <div class="info">
+            <div class="title">
+              景区活动
+            </div>
+            <div class="des">
+              从云台山开始,感受大自然的魅力
+            </div>
+          </div>
+        </div>
       </div>
       <div class="right-bot">时间介绍</div>
     </div>
